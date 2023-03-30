70d7cb981ba0af3cdce1a0eb61070d3d4abbf86e
Sanjeevi <sanjeevimani567@gmail.com>
2023-03-30 11:11:51 +0530

    Update Generalized first occurring character.rs

--- Generalized first occurring character.rs
+++ Generalized first occurring character.rs
@@ -6,11 +6,12 @@ fn main(){
 fn generic_occurring_chars(s:&str,occurring:u8)->Option<char>{
     let mut hashmap =HashMap::new();
     let mut count;
-    
+    //This ensures that keys and values are pushed into the hashmap before any operation on the hashmap
     for cha in s.chars(){
       count = hashmap.entry(cha).or_insert(0);
           *count+=1;
     }
+   //This won't panic since we only unwrapping after the characters are inserted into hashmap.
     for cha in s.chars(){
     if hashmap.get(&cha).unwrap() == &occurring{
             return Some(cha.clone());
