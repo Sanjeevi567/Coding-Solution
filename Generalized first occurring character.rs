5e9665a5d8934ad2db0b04e43886080587dfb626
Sanjeevi <sanjeevimani567@gmail.com>
2023-03-30 11:08:48 +0530

    Create Generalized first occurring character.rs

---
+++ Generalized first occurring character.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+fn main(){
+   //This is generalized version of first_ocuuring character
+    println!("{:?}",generic_occurring_chars(&"BCBA",2));
+}
+fn generic_occurring_chars(s:&str,occurring:u8)->Option<char>{
+    let mut hashmap =HashMap::new();
+    let mut count;
+    
+    for cha in s.chars(){
+      count = hashmap.entry(cha).or_insert(0);
+          *count+=1;
+    }
+    for cha in s.chars(){
+    if hashmap.get(&cha).unwrap() == &occurring{
+            return Some(cha.clone());
+        }
+        }
+    None
+    
+}
